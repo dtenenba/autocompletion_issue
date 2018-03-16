f4c761acee0d63c1290767d2154a4a81d111b7f4
Dan Tenenbaum <dtenenba@fhcrc.org>
2018-03-16 11:42:44 -0700

    add more code

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func main() {
@@ -12,6 +15,24 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(cfg)
-	// On the next line, try typing "cfg." and see if any autocompletion happens
+
+	cfg.Region = endpoints.UsWest2RegionID
+	svc := s3.New(cfg)
+	input := &s3.ListObjectsV2Input{
+		Bucket:  aws.String("mybucket"),
+		MaxKeys: aws.Int64(1000),
+	}
+	// var keys []string
+
+	for {
+		req := svc.ListObjectsV2Request(input)
+		result, err := req.Send()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(result)
+		// on the next line, type `result.` and see if there are autocompletions
+
+	}
 
 }
